422431796be5ce3d957a560078aaa92e12125da6
Taner Incirci <99971373+incirci@users.noreply.github.com>
2026-08-06 12:23:03 +0200

    feat: expose edge inset in preferences

--- src/prefs.js
+++ src/prefs.js
@@ -1,5 +1,6 @@
 import Adw from 'gi://Adw';
 import Gio from 'gi://Gio';
+import Gtk from 'gi://Gtk';
 
 import {ExtensionPreferences} from 'resource:///org/gnome/Shell/Extensions/js/extensions/prefs.js';
 
@@ -21,6 +22,19 @@ export default class MonitorPointerLockPreferences extends ExtensionPreferences 
         });
         settings.bind('enabled', enabled, 'active', Gio.SettingsBindFlags.DEFAULT);
         group.add(enabled);
+
+        const edgeInset = new Adw.SpinRow({
+            title: 'Edge inset',
+            subtitle: 'Distance inside each monitor where crossing is blocked',
+            adjustment: new Gtk.Adjustment({
+                lower: 1,
+                upper: 64,
+                step_increment: 1,
+                page_increment: 4,
+            }),
+        });
+        settings.bind('edge-inset', edgeInset, 'value', Gio.SettingsBindFlags.DEFAULT);
+        group.add(edgeInset);
         page.add(group);
         window.add(page);
     }
